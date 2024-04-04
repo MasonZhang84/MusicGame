3e0a9b44f6818e2c9cbf23f5401c3bc3263360ab
Arian Zohourfazeli <70610995+Arianzf@users.noreply.github.com>
2024-04-04 05:49:12 -0400

    Demo Number 7
    Implemented the respawn screen from previous versions and added 3 big rooms. Fixed respawn and restart button

---
+++ MusicGame-main-N7/MusicMaze/Assets/Scrips/GameOverScreen.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+using TMPro; // This is necessary for TextMeshProUGUI
+using UnityEngine.SceneManagement;
+
+public class GameOverScreen : MonoBehaviour
+{
+    public TextMeshProUGUI gameOverKillCountText; 
+
+    public void Start()
+    {
+        // This line will deactivate the Game Over UI when the game starts
+        gameObject.SetActive(false);
+    }
+
+    public void RestartGame()
+    {
+        // Reload the current scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        // Reset the kill count explicitly
+        KillCounting.ResetKills();
+
+        // Resume the game in case it was paused
+        Time.timeScale = 1;
+
+
+    }
+
+    public void Respawn()
+    {
+        // Reload the current scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+
+        // Resume the game in case it was paused
+        Time.timeScale = 1;
+
+
+    }
+
+
+    public void OnGameOver()
+    {
+        gameOverKillCountText.text = "Kills: " + KillCounting.kills.ToString(); // Assuming KillCounter.kills is your static kill count variable
+        gameObject.SetActive(true); // Activate the Game Over screen
+    }
+
+    // Add this method to handle the quit action
+    public void QuitGame()
+    {
+        // If we are running in a standalone build of the game
+        #if UNITY_STANDALONE
+        Application.Quit();
+        #endif
+
+        // If we are running in the editor
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+}
